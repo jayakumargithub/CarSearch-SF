fb83cba9fbeabd265e841c43bd90e3cedfdb95c9
Jayakumar <jayakumar.m.h@gmail.com>
2020-03-28 22:52:55 +0000

    cartype, car, carexperience added

---
+++ RentACar/force-app/main/default/aura/CarSearchForm/CarSearchFormController.js
@@ -0,0 +1,5 @@
+({
+    doInit : function(component, event, helper) {
+    helper.getCarType(component, event, helper);  
+    }
+})
---
+++ RentACar/force-app/main/default/aura/RentACarHost/RentACarHost.app
@@ -0,0 +1,4 @@
+<aura:application>
+<c:CarSearchForm />
+
+</aura:application>	
